94e7302047a06016964d648c027156cf03e6d737
cynthialu42 <clu2334@gmail.com>
2018-08-26 20:54:09 -0500

    added profiles after the quiz

---
+++ birdbuddy/src/BirdProfileData.js
@@ -0,0 +1,20 @@
+const data = {
+    cockatiel: {
+        description: "A very nice pikachu bird",
+        image: "https://img.birdsnow.com/slir/w800/birds/data/photos/520867/1492549707/white-cockatiel-for-sale.jpg"
+    },
+    budgie: {
+        description: "Small fast blorb",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Melopsittacus_undulatus_-Fort_Worth_Zoo-8a-4c.jpg/220px-Melopsittacus_undulatus_-Fort_Worth_Zoo-8a-4c.jpg"
+    },
+    conure:{
+        description: "Loud cuddle boy",
+        image: "http://loveyourparrot.com/yahoo_site_admin/assets/images/sun_conure_full_body.293222103_std.jpg"
+    },
+    macaw: {
+        description: "Danger calm boy",
+        image: "https://download.ams.birds.cornell.edu/api/v1/asset/51039661/large"
+    }
+}
+
+export default data;
--- birdbuddy/src/components/Profile.js
+++ birdbuddy/src/components/Profile.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { showBird } from '../actions';
+import data from '../BirdProfileData';
 
 class Profile extends Component {
+    
     render(){
+        var url = window.location.pathname;
+        var splitUrl = url.split('/');
+        var searchTerm = splitUrl.pop();
+
         console.log('Profile results: ',this.props.results);
+        console.log(data[searchTerm].description);
         return(
-            <div>{this.props.results.breed}</div>
+            <div>
+                <div>{searchTerm}</div>
+                <div>Description: {data[searchTerm].description}</div>
+                <img src = {data[searchTerm].image} width="100"/>
+            </div>
         )
     }
 }
